874c95fffdeb51c59b1fc1ab745eaebb7982d73f
jsp <zinsani@gmail.com>
2024-04-19 22:21:13 +0900

    입영준비 사항02 퍼블리싱

--- src/pages/home/enlist/ReadyEnlist01.vue
+++ src/pages/home/enlist/ReadyEnlist01.vue
@@ -4,22 +4,22 @@
   <q-page class="px-6 flex flex-col">
     <div class="btn-group mt-[15px] flex gap-[5px]">
       <div
-        class="text-pretendard level-btn w-[78px] text-center text-base leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
+        class="level-btn w-[78px] text-center text-[13px] font-medium leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
       >
         장교
       </div>
       <div
-        class="text-pretendard level-btn w-[78px] text-center text-base leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
+        class="level-btn w-[78px] text-center text-[13px] font-medium leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
       >
         부사관
       </div>
       <div
-        class="text-pretendard level-btn w-[78px] text-center text-base leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
+        class="level-btn w-[78px] text-center text-[13px] font-medium leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
       >
         병사
       </div>
       <div
-        class="text-pretendard level-btn w-[78px] text-center text-base leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
+        class="level-btn w-[78px] text-center text-[13px] font-medium leading-[40px] h-[40px] !border border-grey-1 rounded-[20px]"
       >
         유의사항
       </div>
@@ -31,4 +31,10 @@
   </q-page>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.level-btn:hover {
+  background-color: #196af5;
+  color: #fff;
+  border: none;
+}
+</style>
