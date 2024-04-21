b7ad8a85f6b9514514e49192494bd350ff95b00b
jsp <zinsani@gmail.com>
2024-04-21 21:05:32 +0900

    p44-45 자주묻는질문 퍼블리싱

--- src/pages/home/enlist/FaQuestion.vue
+++ src/pages/home/enlist/FaQuestion.vue
@@ -3,79 +3,236 @@
 <template>
   <q-page class="px-6 pt-[22px]">
     <div
-      class="faq-category grid grid-cols-2 place-items-center h-[120px] border border-grey-1"
+      class="faq-category grid grid-cols-2 place-items-center h-[120px] border border-grey-1 bg-grey gap-[1px]"
     >
-      <div class="text-grey-4 text-base font-pretendard hover:text-primary">
+      <div
+        class="text-grey-4 text-base font-pretendard hover:text-primary size-full flex justify-center items-center bg-white"
+      >
         편지
       </div>
-      <div class="text-grey-4 text-base font-pretendard hover:text-primary">
+      <div
+        class="text-grey-4 text-base font-pretendard hover:text-primary size-full flex justify-center items-center bg-white"
+      >
         외출/휴가
       </div>
-      <div class="text-grey-4 text-base font-pretendard hover:text-primary">
+      <div
+        class="text-grey-4 text-base font-pretendard hover:text-primary size-full flex justify-center items-center bg-white"
+      >
         특기/배속
       </div>
-      <div class="text-grey-4 text-base font-pretendard hover:text-primary">
+      <div
+        class="text-grey-4 text-base font-pretendard hover:text-primary size-full flex justify-center items-center bg-white"
+      >
         면회
       </div>
-      <div class="text-grey-4 text-base font-pretendard hover:text-primary">
+      <div
+        class="text-grey-4 text-base font-pretendard hover:text-primary size-full flex justify-center items-center bg-white"
+      >
         훈련병
       </div>
-      <div class="text-grey-4 text-base font-pretendard hover:text-primary">
+      <div
+        class="text-grey-4 text-base font-pretendard hover:text-primary size-full flex justify-center items-center bg-white"
+      >
         기타
       </div>
     </div>
 
     <div class="mt-6 w-screen left-0 absolute h-[10px] bg-grey"></div>
 
-    <section class="mt-6">
+    <section class="mt-6 mb-[9px]">
       <h3 class="py-[25px] text-sm font-medium text-grey-4">
         자주 묻는 질문 top 10
       </h3>
       <ul>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
+        </li>
+
+        <li
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+        >
+          1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
         <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
+          class="faq-list py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[7%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
         >
           1. 질문 1은 질문1 질문12 질문 질문문?
-        </li>
-        <li
-          class="py-[25px] border-b border-grey-1 relative text-[13px] after:absolute after:right-0 after:top-[-30%] after:content-[''] after:size-[30px] after:bg-no-repeat after:bg-cover after:bg-[url('/src/assets/icons/down_arrow.svg/')]"
-        >
-          1. 질문 1은 질문1 질문12 질문 질문문?
+          <div
+            class="faq-detail hidden bg-grey p-6 w-screen -translate-x-6 translate-y-[25px]"
+          >
+            <p class="text-sm font-pretendard">
+              1. 질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문
+              질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1 질문12
+              질문 질문문?질문 1은 질문1 질문12 질문 질문문?질문 1은 질문1
+              질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문? 질문 1은
+              질문1 질문12 질문 질문문?질문 1은 질문1 질문12 질문 질문문?
+            </p>
+            <p
+              class="relative underline underline-offset-2 pl-[17px] mt-10 font-pretendard text-sm text-primary before:absolute before:top-[20%] before:left-0 before:content-[''] before:w-[9px] before:h-3 before:bg-[url('/src/assets/icons/icon_file.svg')]"
+            >
+              <a href="">공군메타버스 훈련자료[다운로드]</a>
+            </p>
+          </div>
         </li>
       </ul>
     </section>
