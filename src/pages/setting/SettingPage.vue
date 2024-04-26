2e107592593ebcbf3e21806b916e3ed649931833
jsp <zinsani@gmail.com>
2024-04-26 10:48:16 +0900

    설정페이지 리팩터링

--- src/pages/setting/SettingPage.vue
+++ src/pages/setting/SettingPage.vue
@@ -3,10 +3,9 @@
   <q-page>
     <router-link
       :to="{ name: '' }"
-      class="w-full h-[67px] pl-6 pr-3 flex items-center"
+      class="w-full h-[67px] pl-6 pr-3 flex justify-between items-center"
     >
       <a-subtitle>계정 설정</a-subtitle>
-      <div class="flex-1"></div>
       <q-icon
         name="img:/src/assets/icons/icon_arrow_set.svg"
         size="30px"
@@ -15,19 +14,18 @@
 
     <!-- 서비스 약관 -->
     <div
-      class="pl-6 flex items-center w-full h-[30px] bg-grey font-pretendard text-caption text-grey-4"
+      class="pl-6 py-2 w-full bg-grey font-pretendard text-caption text-grey-4 leading-[14px]"
     >
       서비스 약관
     </div>
 
     <router-link
       :to="{ name: '' }"
-      class="w-full h-[67px] pl-6 pr-3 flex items-center border-b border-grey-1"
+      class="w-full h-[67px] pl-6 pr-3 flex justify-between items-center border-b border-grey-1"
     >
       <h3 class="text-sm font-pretendard font-medium text-secondary">
         서비스 이용약관
       </h3>
-      <div class="flex-1"></div>
       <q-icon
         name="img:/src/assets/icons/icon_arrow_set.svg"
         size="30px"
@@ -35,12 +33,11 @@
     </router-link>
     <router-link
       :to="{ name: '' }"
-      class="w-full h-[67px] pl-6 pr-3 flex items-center"
+      class="w-full h-[67px] pl-6 pr-3 flex justify-between items-center"
     >
       <h3 class="text-sm font-pretendard font-medium text-secondary">
         개인정보 처리방침
       </h3>
-      <div class="flex-1"></div>
       <q-icon
         name="img:/src/assets/icons/icon_arrow_set.svg"
         size="30px"
@@ -49,28 +46,26 @@
 
     <!-- 고객 지원 -->
     <div
-      class="pl-6 flex items-center w-full h-[30px] bg-grey font-pretendard text-caption text-grey-4"
+      class="pl-6 py-2 w-full bg-grey font-pretendard text-caption text-grey-4 leading-[14px]"
     >
       고객 지원
     </div>
 
     <div
-      class="w-full h-[67px] pl-6 pr-3 flex items-center border-b border-grey-1"
+      class="w-full h-[67px] pl-6 pr-3 flex justify-between items-center border-b border-grey-1"
     >
       <h3 class="text-sm font-pretendard font-medium text-secondary">
         버전정보
       </h3>
-      <div class="flex-1"></div>
       <p class="pr-3 font-pretendard text-sm text-primary">n.n.n</p>
     </div>
     <router-link
       :to="{ name: 'notice-list' }"
-      class="w-full h-[67px] pl-6 pr-3 flex items-center border-b border-grey-1"
+      class="w-full h-[67px] pl-6 pr-3 flex justify-between items-center border-b border-grey-1"
     >
       <h3 class="text-sm font-pretendard font-medium text-secondary">
         공지사항
       </h3>
-      <div class="flex-1"></div>
       <q-icon
         name="img:/src/assets/icons/icon_arrow_set.svg"
         size="30px"
@@ -78,12 +73,11 @@
     </router-link>
     <router-link
       :to="{ name: 'cancel-membership' }"
-      class="w-full h-[67px] pl-6 pr-3 flex items-center"
+      class="w-full h-[67px] pl-6 pr-3 flex justify-between items-center"
     >
       <h3 class="text-sm font-pretendard font-medium text-secondary">
         회원탈퇴
       </h3>
-      <div class="flex-1"></div>
       <q-icon
         name="img:/src/assets/icons/icon_arrow_set.svg"
         size="30px"
