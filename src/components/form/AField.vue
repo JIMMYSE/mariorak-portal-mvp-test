a033d787dd8d9f0c00d65e41eead76839412f4d8
SD-LJW <jay8126@gmail.com>
2024-06-21 20:31:58 +0900

    feat: 폼 컴포넌트 디자인 수정

--- src/components/form/AField.vue
+++ src/components/form/AField.vue
@@ -1,3 +1,5 @@
+<!-- 컴포넌트 > 폼 > 입력 필드 -->
+
 <script setup lang="ts">
 type Props = {
   label: string;
@@ -6,8 +8,8 @@ defineProps<Props>();
 </script>
 
 <template>
-  <section class="mt-[20px]">
-    <div class="text-caption text-grey-4 leading-5 pl-[2px]">{{ label }}</div>
+  <section class="mt-4">
+    <div class="text-[13px] text-grey-3 leading-8 pl-1">{{ label }}</div>
     <slot />
     <slot name="bottom" />
   </section>
