7a590abdc5565e5d351adbc9ea17edb6355a8505
SD-LJW <jay8126@gmail.com>
2024-06-25 18:05:52 +0900

    feat: 컨텐트 다이얼로그 수정

--- src/components/common/ADialogContent.vue
+++ src/components/common/ADialogContent.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useDialogPluginComponent } from 'quasar';
-import { toValue } from 'vue';
 import { ADialogContentProps } from './ADialogContent';
 
 const props = withDefaults(defineProps<ADialogContentProps>(), {
@@ -17,22 +15,24 @@ const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
 
 <template>
   <q-dialog ref="dialogRef" persistent maximized @hide="onDialogHide">
-    <q-card class="fit col">
-      <q-card-section class="row justify-between items-center">
-        <div />
-        <div class="text-[18px] font-medium">
-          {{ toValue(title) }}
-        </div>
-        <ABtn icon="close" flat round dense v-close-popup />
-      </q-card-section>
+    <q-card class="flex flex-col flex-nowrap p-2" flat>
       <q-card-section
-        data-cy-id="text"
-        class="col-grow text-grey-4 q-px-xl q-pb-xl text-caption"
-        style="word-break: break-all; color: #888888"
+        class="sticky top-0 flex justify-center items-start h-12 py-1"
       >
-        <q-scroll-area style="height: 500px; max-width: 500px">
+        <div class="text-[17px]">
+          {{ title }}
+        </div>
+        <q-icon
+          class="absolute top-0 right-2 cursor-pointer"
+          name="img:/src/assets/icons/btn_delete.svg"
+          size="30px"
+          v-close-popup
+        />
+      </q-card-section>
+      <q-card-section class="flex-1 font-pretendard text-[14px] pb-8">
+        <q-scroll-area class="size-full">
           <div v-if="html" v-html="html" />
-          <div v-if="text">{{ text }}</div>
+          <div v-if="text" class="whitespace-pre-wrap">{{ text }}</div>
         </q-scroll-area>
       </q-card-section>
     </q-card>
