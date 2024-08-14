6883411aa1c6a22385082a5aeccf88006317af68
TonySon8245 <dongo8245@gmail.com>
2024-08-14 12:13:21 +0900

    fix : 잘못된 경로 수정

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -3,7 +3,7 @@ import * as Qs from 'qs';
 import { LoadingBar } from 'quasar';
 import { boot } from 'quasar/wrappers';
 import { getAccessToken } from 'src/composables/domain/auth';
-import { handleAxiosError } from 'src/services/util/error-handler';
+import { handleAxiosError } from 'src/services/error-handler';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
