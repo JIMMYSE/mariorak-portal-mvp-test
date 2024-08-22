984f3d2a001e63cf98970d6d15c6ada9e608d61c
Liche <dev.lovelylove@gmail.com>
2024-08-22 18:48:46 +0900

    feat: faq 수정

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -41,6 +41,45 @@ const routes: RouteRecordRaw[] = [
             },
             component: () => import('pages/game-pack/MainPage.vue'),
           },
+          // 게임 목록/상세
+          {
+            path: 'game/list',
+            name: 'game-list',
+            meta: {
+              title: '게임 목록',
+              bgColor: 'white',
+            },
+            component: () => import('pages/game-pack/game/GameList.vue'),
+          },
+          {
+            path: 'game/:id',
+            name: 'game-detail',
+            meta: {
+              title: '게임 상세',
+              bgColor: 'white',
+            },
+            component: () => import('pages/game-pack/game/GameDetail.vue'),
+          },
+          // 프로젝트 목록/상세
+          {
+            path: 'project/list',
+            name: 'project-list',
+            meta: {
+              title: '프로젝트 목록',
+              bgColor: 'white',
+            },
+            component: () => import('pages/game-pack/project/ProjectList.vue'),
+          },
+          {
+            path: 'project/:id',
+            name: 'project-detail',
+            meta: {
+              title: '프로젝트 상세',
+              bgColor: 'white',
+            },
+            component: () =>
+              import('pages/game-pack/project/ProjectDetail.vue'),
+          },
         ],
       },
       {
