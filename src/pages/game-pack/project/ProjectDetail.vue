17116fee17b11bc49def3e36b2b65a95b54a576a
Liche <dev.lovelylove@gmail.com>
2024-08-26 18:42:44 +0900

    feat: 프로젝트 상세 퍼블리싱 진행 중

--- src/pages/game-pack/project/ProjectDetail.vue
+++ src/pages/game-pack/project/ProjectDetail.vue
@@ -1,4 +1,15 @@
 <script lang="ts" setup></script>
 <template>
-  <div></div>
+  <q-page>
+    <section>
+      <div class="h-[210px] w-full">
+        <q-icon
+          name="img:/icons/icon_in_progress.svg"
+          size="50px"
+          class="absolute z-10 ml-[13px]"
+        />
+        <q-img src="/images/dummy/game_dummy.svg" width="100%" />
+      </div>
+    </section>
+  </q-page>
 </template>
