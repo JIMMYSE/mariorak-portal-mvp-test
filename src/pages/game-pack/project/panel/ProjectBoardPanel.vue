cacee19fcae95bade704fd165497eeb94c5bc647
Liche <dev.lovelylove@gmail.com>
2024-08-30 18:45:43 +0900

    feat: 게시판 퍼블리싱 진행 중

---
+++ src/pages/game-pack/project/panel/ProjectBoardPanel.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const searchSort = ref('latest');
+const options = [
+  {
+    label: '최신순',
+    value: 'latest',
+  },
+  {
+    label: '인기순',
+    value: 'popular',
+  },
+];
+</script>
+<template>
+  <div class="w-full">
+    <section class="mt-6">
+      <c-search-input name="keyword" />
+      <c-select
+        v-model="searchSort"
+        :options="options"
+        map-options
+        borderless
+        class="w-[80px]"
+        dense
+      />
+    </section>
+  </div>
+</template>
