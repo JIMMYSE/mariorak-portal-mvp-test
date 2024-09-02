4dad7e13b58358d322d6106955b090b8fc36b414
Liche <dev.lovelylove@gmail.com>
2024-09-02 10:20:07 +0900

    feat: 프로젝트 상세 > 게시판 퍼블리싱

--- src/pages/game-pack/project/panel/ProjectBoardPanel.vue
+++ src/pages/game-pack/project/panel/ProjectBoardPanel.vue
@@ -14,7 +14,20 @@ const options = [
 <template>
   <div class="w-full">
     <section class="mt-6">
-      <c-search-input name="keyword" />
+      <div class="flex justify-between items-center">
+        <div>
+          <p class="text-[#222222] text-xl font-semibold leading-7">
+            개발자 게시판 (135)
+          </p>
+          <p class="text-[#767676] text-sm font-normal leading-tight mt-[2px]">
+            프로젝트에 참여한 개발자가 작성하는 게시판
+          </p>
+        </div>
+        <q-btn size="md" round flat class="flex justify-center items-center">
+          <q-icon name="img:/icons/icon_add_plus.svg" size="40px" />
+        </q-btn>
+      </div>
+      <c-search-input name="keyword" class="mt-[16px]" />
       <c-select
         v-model="searchSort"
         :options="options"
@@ -23,6 +36,12 @@ const options = [
         class="w-[80px]"
         dense
       />
+      <div>
+        <!-- 반복문 -->
+        <g-p-board-comment-item />
+        <g-p-board-comment-item />
+        <g-p-board-comment-item />
+      </div>
     </section>
   </div>
 </template>
