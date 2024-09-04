3dd7d168a6087e2dbfebbbb291bfcd4abc35b946
TonySon8245 <dongo8245@gmail.com>
2024-09-04 17:37:57 +0900

    feat : 브릿지에 프로젝트 연결

--- src/pages/game-pack/project/panel/ProjectInfoPanel.vue
+++ src/pages/game-pack/project/panel/ProjectInfoPanel.vue
@@ -18,13 +18,14 @@ const props = defineProps<Props>();
         <introduce-text :intro="detail?.cont" />
       </div>
     </section>
-    <section class="mt-[60px]">
+    <!-- TODO 앱심사 히든처리 -->
+    <!-- <section class="mt-[60px]" v-if="false">
       <h2 class="text-[20px] font-semibold">In Game Shot</h2>
       <div class="mt-4" v-if="detail?.detail_file_list">
         <game-shot-movie :detail-file-list="detail?.detail_file_list" />
       </div>
       <div v-else></div>
-    </section>
+    </section> -->
     <section class="mt-[40px]">
       <h2 class="text-[20px] font-semibold">프로젝트 참여 멤버</h2>
       <div class="mt-4">
