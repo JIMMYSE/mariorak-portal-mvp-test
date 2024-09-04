2e22217b43a6e040f511f8616ad0ce64a81c6333
Liche <dev.lovelylove@gmail.com>
2024-09-04 13:45:33 +0900

    feat: 회원가입, 탈퇴 다이얼로그 추가

--- src/pages/my-page/AccountManage.vue
+++ src/pages/my-page/AccountManage.vue
@@ -1,4 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const logout = () => {
+  useMyConfirmDialog({
+    htmlText: `회원 닉네임 님, <br/> 정말 로그아웃 하시겠습니까?`,
+  }).onOk(() => {
+    // logout
+  });
+};
+
+const withdrawal = () => {
+  useMyConfirmDialog({
+    htmlText: `회원 닉네임 님, <br/> 정말 탈퇴하시겠습니까?`,
+  }).onOk(() => {
+    // logout
+  });
+};
+</script>
 <template>
   <q-page>
     <section class="px-6 pt-8">
@@ -27,12 +43,14 @@
           class="text-[#767676] text-base font-medium leading-tight p-0"
           flat
           label="로그아웃"
+          @click="logout"
         />
         <q-separator class="text-[#f0f0f0] my-[8px]" vertical inset />
         <q-btn
           flat
           class="text-[#ff0000]/50 text-base font-medium leading-tight p-0"
           label="회원탈퇴"
+          @click="withdrawal"
         />
       </div>
     </section>
