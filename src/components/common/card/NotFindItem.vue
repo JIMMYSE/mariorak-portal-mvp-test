e06284f1ba1b290833dfdd13b0947f4069e414bf
Liche <dev.lovelylove@gmail.com>
2024-09-11 10:16:33 +0900

    feat: 아이템 없을 시 화면 퍼블리싱

---
+++ src/components/common/card/NotFindItem.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+type Props = {
+  itemName: string;
+};
+
+const props = defineProps<Props>();
+
+const checkKorean = (name: string) => {
+  const lastChar = name.charCodeAt(name.length - 1);
+  const isThereLastChar = (lastChar - 0xac00) % 28;
+  if (isThereLastChar) {
+    return '이';
+  }
+  return '가';
+};
+</script>
+<template>
+  <div>
+    <section class="flex justify-center items-center flex-col">
+      <q-img src="/images/join_failed.svg" width="120px" height="120px" />
+      <q-img
+        src="/images/icon_shadow.svg"
+        width="110px"
+        height="25px"
+        class="mt-[5px]"
+      />
+    </section>
+    <p
+      class="mt-[40px] text-center text-[#767676] text-base font-normal leading-snug"
+    >
+      해당 검색어에 맞는 {{ props.itemName
+      }}{{ checkKorean(props.itemName) }} 없어요
+    </p>
+  </div>
+</template>
