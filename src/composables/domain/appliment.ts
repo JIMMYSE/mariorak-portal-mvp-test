4758d05458148ac229cfb1cb274807dbdf28cceb
RoyLee <keymasroy@gmail.com>
2024-09-19 17:36:12 +0900

    feat: 프로젝트 지원 요청 내역 상세 팝업 추가

--- src/composables/domain/appliment.ts
+++ src/composables/domain/appliment.ts
@@ -1,17 +1,36 @@
-import { ProjectAppliementDetailResType } from 'src/types/gamepack/recruit-model';
+import { ProjectAppliementDetailListResType, ProjectAppliementDetailResType } from 'src/types/gamepack/recruit-model';
+
 import { MaybeRefOrGetter } from 'vue';
 import { QueryOption } from 'src/types/common/api-model';
 
 const API_URL = '/v3/pr/appliment';
 const QUERY_KEY = {
+  REQUEST_DETAIL: 'recruit-appliment-request-detail',
   DETAIL: 'recruit-appliment-detail',
 };
 
 /**
  * 로그인 사용자의 지원 목록 조회
  */
-export const useRecruitApplymentDetail = (queryOption?: QueryOption) => {
-  return useQueryFetch<ProjectAppliementDetailResType>({ url: API_URL, queryKeyName: QUERY_KEY.DETAIL, queryOption });
+export const useRecruitApplymentRequestDetail = (queryOption?: QueryOption) => {
+  return useQueryFetch<ProjectAppliementDetailListResType>({
+    url: API_URL,
+    queryKeyName: QUERY_KEY.REQUEST_DETAIL,
+    queryOption,
+  });
+};
+
+/**
+ * 프로젝트 지원 상세 정보 조회
+ * @param prjAplyId 프로젝트 지원 아이디
+ */
+export const useRecruiteApplyment = (prjAplyId: MaybeRefOrGetter<number>, queryOption?: QueryOption) => {
+  return useQueryFetchItem<ProjectAppliementDetailResType>({
+    url: API_URL,
+    id: prjAplyId,
+    queryKeyName: QUERY_KEY.REQUEST_DETAIL,
+    queryOption,
+  });
 };
 
 /**
@@ -38,5 +57,14 @@ export const cancelRecruitApplyment = (prj_rcrt_id: number) => {
 export const rejectRecruitApplyment = (prj_rcrt_id: number) => {
   return useAxiosPatch<ApiResponse>({
     url: API_URL + '/reject/' + prj_rcrt_id,
+  });
+};
+
+/**
+ * 지원 수락
+ */
+export const approveRecruitApplyment = (prj_rcrt_id: number) => {
+  return useAxiosPatch<ApiResponse>({
+    url: API_URL + '/approve/' + prj_rcrt_id,
   });
 };
--- src/pages/recruit/panel/RecruitDetailApplyPanel.vue
+++ src/pages/recruit/panel/RecruitDetailApplyPanel.vue
@@ -7,7 +7,7 @@ const {
   data: applimentData,
   isFetched,
   refetch,
-} = useRecruitApplymentDetail({
+} = useRecruitApplymentRequestDetail({
   staleTime: 0,
 });
 
@@ -85,6 +85,11 @@ const appliedProjectAction = createAction(() => {
 const requestedAction = createAction(() => {
   const menuOpenStates = ref([]);
 
+  /**
+   * 확인 팝업 컴포넌트
+   */
+  const confirmPopup = shallowRef<InstanceType<typeof RecruitConfirm>>();
+
   watch(
     isFetched,
     (v) => {
@@ -113,7 +118,14 @@ const requestedAction = createAction(() => {
    * @param prj_aply_id
    */
   function checkRequestedContent(prj_aply_id: number) {
-    notAvailableAlert();
+    // notAvailableAlert();
+    confirmPopup.value.open(prj_aply_id, {
+      onClosed(isUpdated) {
+        if (isUpdated) {
+          refetch();
+        }
+      },
+    });
   }
 
   /**
@@ -144,6 +156,7 @@ const requestedAction = createAction(() => {
     toggleMenu,
     checkRequestedContent,
     rejectAppliment,
+    confirmPopup,
   };
 });
 // #endregion
@@ -240,7 +253,7 @@ const toggleApplyDropdown = (event: Event) => {
     <section class="px-6 mt-[30px]">
       <p class="text-[#222222] text-xl font-semibold leading-7">프로젝트 요청내역</p>
 
-      <RecruitConfirm />
+      <RecruitConfirm :ref="(comp: InstanceType<typeof RecruitConfirm>) => requestedAction.confirmPopup.value = comp" />
       <template v-if="isFetched && applimentData.requestedList.length > 0">
         <div class="mt-[24px]">
           <ul>
@@ -293,12 +306,24 @@ const toggleApplyDropdown = (event: Event) => {
                           >
                             <q-list style="min-width: 120px">
                               <q-item clickable v-ripple>
-                                <q-item-section @click="requestedAction.checkRequestedContent(item.prj_aply_id)"
+                                <q-item-section
+                                  @click="
+                                    () => {
+                                      requestedAction.checkRequestedContent(item.prj_aply_id);
+                                      requestedAction.menuOpenStates.value[index] = false;
+                                    }
+                                  "
                                   >신청내용 확인</q-item-section
                                 >
                               </q-item>
                               <q-item clickable v-ripple>
-                                <q-item-section @click="requestedAction.rejectAppliment(item.prj_aply_id)"
+                                <q-item-section
+                                  @click="
+                                    () => {
+                                      requestedAction.rejectAppliment(item.prj_aply_id);
+                                      requestedAction.menuOpenStates.value[index] = false;
+                                    }
+                                  "
                                   >거절하기</q-item-section
                                 >
                               </q-item>
@@ -328,7 +353,7 @@ const toggleApplyDropdown = (event: Event) => {
         <not-find-item message="프로젝트 요청 내역이 없어요." />
       </div>
 
-      <div>
+      <div v-if="false">
         <ul>
           <li
             class="flex flex-col relative cursor-pointer"
