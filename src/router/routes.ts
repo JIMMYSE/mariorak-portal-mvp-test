f7326478f51ad2e2ccf2ea843675291a1faab1e7
Liche <dev.lovelylove@gmail.com>
2024-09-19 17:33:30 +0900

    hotfix: 로그인 화면 푸터 제거

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -263,6 +263,7 @@ const routes: RouteRecordRaw[] = [
           title: '로그인',
           requiresNonAuth: true,
           noHeader: true,
+          noFooter: true,
         },
         component: () => import('pages/auth/LoginPage.vue'),
       },
