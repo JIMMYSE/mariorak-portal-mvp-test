8e0c92cdfbfabe2d13e261a8faf02d4a3a8983cb
Liche <dev.lovelylove@gmail.com>
2024-09-24 13:47:56 +0900

    feat: 커뮤니티 작업 중

--- src/pages/community/CMMainPage.vue
+++ src/pages/community/CMMainPage.vue
@@ -21,20 +21,26 @@ const recommendedProjectList = computed(() => {
     <RecruitMainKvBanner />
 
     <!-- 인력사무소 intro -->
-    <RecruitMainIntro />
-    <hr class="h-2.5 bg-[#f7f7f7] mt-10" />
+    <section class="mt-10">
+      <p class="pl-6 text-[22px] font-semibold">COMMUNITY</p>
+      <p class="pl-6 text-[#767676] text-sm font-normal leading-tight">
+        즐거움과 함께 유익한 정보와 교류가 넘쳐나는 이곳은 CCF 커뮤니티 세상
+      </p>
+    </section>
+    <hr class="h-2.5 bg-[#f7f7f7] mt-8" />
 
     <!-- CCF 추천 모집중 프로젝트 -->
-    <section class="mt-10">
-      <h2 class="pl-6 text-[22px] font-semibold">CCF 모집중 추천프로젝트</h2>
+    <section class="mt-[70px]">
+      <h2 class="pl-6 text-[22px] font-semibold">소식통</h2>
+      <p class="pl-6 text-[#767676] text-sm font-normal leading-tight">CCF 크루가 직접 소개하고 알려 드립니다!</p>
       <div class="grid gap-1.5 mt-4">
-        <recruit-recommened-project :p-list="recommendedProjectList" />
+        <g-p-item-list />
       </div>
     </section>
 
     <!-- CCF 추천 개발 인력 -->
     <section class="mt-[55px]">
-      <h2 class="pl-6 text-[22px] font-semibold">CCF 추천 개발 인력</h2>
+      <h2 class="pl-6 text-[22px] font-semibold">최근 게시글</h2>
       <div class="grid gap-1.5 mt-4">
         <recruit-recommened-maker :m-list="recommendedMakerList" />
       </div>
@@ -47,8 +53,8 @@ const recommendedProjectList = computed(() => {
 
     <!-- FAQ -->
     <section class="mt-[55px] mb-[50px]">
-      <h2 class="text-[22px] font-semibold px-6">FAQ</h2>
-      <faq-area />
+      <h2 class="text-[22px] font-semibold px-6">컨퍼런스</h2>
+      <recommend-project-list :gp-list="recommendedProjectList" />
     </section>
   </q-page>
 </template>
